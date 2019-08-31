a9cb9c1d345dbd85b5ce3f6b882ad1313c43d958
Pravesh Tora <pravesh.tora@gmail.com>
2019-08-31 19:57:26 +0530

    feat: add jsonwebtoken middleware

--- back-end/app.js
+++ back-end/app.js
@@ -6,20 +6,43 @@ const mongoose = require('mongoose');
 const app = express();
 const graphqlSchema = require('./schema');
 const resolver = require('./resolvers/index');
+const jwt = require('express-jwt');
 const port = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/hello', (req, res, next) => res.send({ message: 'hello ScoutBase' }));
+// authentication middleware,sets req.user
+const authMiddleware = jwt({
+  secret: 'secret',
+  credentialsRequired: false,
+  getToken: function fromHeaderOrQuerystring (req) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        return req.headers.authorization.split(' ')[1];
+    } else if (req.query && req.query.token) {
+      return req.query.token;
+    }
+    return null;
+  }
 
-app.use('/graphql', graphqlHttp({
+});
+
+app.use(authMiddleware);
+
+app.use('/graphql', graphqlHttp( req => {
+  return {
   schema: graphqlSchema,
+  context: {
+    user : req.user
+  },
   rootValue: resolver,
   graphiql: true,
-  customFormatErrorFn : (error) => {
-    return ({message: error.message, statusCode : error.statusCode});
+  customFormatErrorFn: (error) => {
+    console.log(error);
+    return ({ message: error.message, statusCode: error.statusCode });
   }
+}
 }));
 
 const CONNECTION_URL = "mongodb://127.0.0.1:27017/movies-db";
